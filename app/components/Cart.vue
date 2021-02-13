91c22dfd8b9d5875021cb1a3843b2c057b27b87a
Marcel Kredzel <marcel.kredzel@gmail.com>
2021-02-13 02:35:35 +0000

    Fixed Shopping Cart Session

--- app/components/Cart.vue
+++ app/components/Cart.vue
@@ -90,7 +90,13 @@
         </TextField>
         <TextField hint="Enter Phone Number" v-model="phone" maxLength="15">
         </TextField>
-
+        <Label
+          v-if="this.errorMessage == true"
+          text="Enter valid name (letters only) and valid UK telephone number above"
+          class="total"
+          style="color: #8b0000"
+          textWrap="true"
+        />
         <Button
           text="Checkout"
           class="btn m-t-20 add-button"
@@ -117,7 +123,7 @@ export default {
       itemsPerRow: 2,
       name: "",
       phone: "",
-      checkoutClicked: false,
+      errorMessage: false,
     };
   },
   methods: {
@@ -125,65 +131,86 @@ export default {
       this.$navigateTo(Home);
     },
     checkout() {
-      this.checkoutClicked = true;
-      console.log(this.cart);
-      let order = {
-        name: this.name,
-        telephone: this.phone,
-        lessons: [],
-        total: 0,
-      };
+      let lettersRegex = /^[a-zA-Z\s]*$/;
+      let telephoneRegex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?(\d{4}|\d{3}))?$/;
+      if (
+        !this.name.match(lettersRegex) ||
+        this.name == "" ||
+        !this.phone.match(telephoneRegex) ||
+        this.phone == ""
+      ) {
+        this.validation = false;
+      } else {
+        this.validation = true;
+      }
 
-      this.cart.forEach((lesson) => {
-        order.total = order.total + lesson.price;
+      if (this.validation == false) {
+        this.errorMessage = true;
+      } else {
+        let order = {
+          name: this.name,
+          telephone: this.phone,
+          lessons: [],
+          total: 0,
+        };
 
-        order.lessons.push({
-          name: lesson.topic,
-          price: lesson.price,
+        this.cart.forEach((lesson) => {
+          order.total = order.total + lesson.price;
+
+          order.lessons.push({
+            name: lesson.topic,
+            price: lesson.price,
+          });
+
+          let classes = this.$store.getters.items;
+
+          classes.forEach((product) => {
+            if (lesson.topic == product.topic) {
+              let updatedLesson = {
+                availableSpaces: product.availableSpaces,
+              };
+
+              fetch(
+                `https://after-school-server.herokuapp.com/collection/lessons/${product._id}`,
+                {
+                  method: "PUT", // set the HTTP method as 'PUT'
+                  headers: {
+                    "Content-Type": "application/json", // set the data type as JSON
+                  },
+                  body: JSON.stringify(updatedLesson), // need to stringify the JSON object
+                }
+              )
+                .then((response) => response.json())
+                .then((responseJSON) => {
+                  console.log("Success:", responseJSON);
+                });
+            }
+          });
         });
 
-        let classes = this.$store.getters.items;
-
-        classes.forEach((product) => {
-          if (lesson.topic == product.topic) {
-            let updatedLesson = {
-              availableSpaces: product.availableSpaces,
-            };
-
-            fetch(
-              `https://after-school-server.herokuapp.com/collection/lessons/${product._id}`,
-              {
-                method: "PUT", // set the HTTP method as 'PUT'
-                headers: {
-                  "Content-Type": "application/json", // set the data type as JSON
-                },
-                body: JSON.stringify(updatedLesson), // need to stringify the JSON object
-              }
-            )
-              .then((response) => response.json())
-              .then((responseJSON) => {
-                console.log("Success:", responseJSON);
-              });
-          }
-        });
-      });
-
-      // set the url to your server and route
-      fetch("https://after-school-server.herokuapp.com/collection/orders", {
-        method: "POST", // set the HTTP method as 'POST'
-        headers: {
-          "Content-Type": "application/json", // set the data type as JSON
-        },
-        body: JSON.stringify(order), // need to stringify the JSON object
-      })
-        .then((response) => response.json())
-        .then((responseJSON) => {
-          console.log("Success:", responseJSON);
-        });
-      this.cart = [];
+        // set the url to your server and route
+        fetch("https://after-school-server.herokuapp.com/collection/orders", {
+          method: "POST", // set the HTTP method as 'POST'
+          headers: {
+            "Content-Type": "application/json", // set the data type as JSON
+          },
+          body: JSON.stringify(order), // need to stringify the JSON object
+        })
+          .then((response) => response.json())
+          .then((responseJSON) => {
+            console.log("Success:", responseJSON);
+          });
+        this.cart = [];
+        this.$store.state.cart = [];
+      }
     },
     remove(index) {
       this.cart.splice(index, 1);
+      this.$store.state.cart.splice(index, 1);
+      this.$store.state.items[index].availableSpaces++;
+    },
+    validateData() {
+      console.log(this.name);
     },
   },
   computed: {
